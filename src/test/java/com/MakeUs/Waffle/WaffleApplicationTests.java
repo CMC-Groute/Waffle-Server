ac46d1a493612afaad1c40948da19bc0ef909771
Chaeonida <46310555+Chaeonida@users.noreply.github.com>
2022-06-09 16:44:53 +0900

    Initial Commit

---
+++ src/test/java/com/MakeUs/Waffle/WaffleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.MakeUs.Waffle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WaffleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
