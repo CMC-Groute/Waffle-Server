61a1c34a277f25e323507b4401216d52d17d6d76
Chaeonida <46310555+Chaeonida@users.noreply.github.com>
2022-07-02 00:18:32 +0900

    :bug: 스케줄러 제대로 동작 안하는 오류 해결

--- src/main/java/com/MakeUs/Waffle/domain/invitation/repository/InvitationRepository.java
+++ src/main/java/com/MakeUs/Waffle/domain/invitation/repository/InvitationRepository.java
@@ -6,6 +6,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDate;
 import java.util.List;
@@ -19,6 +20,7 @@ public interface InvitationRepository extends JpaRepository<Invitation, Long> {
     List<Invitation> getByUser(@Param(value = "userId") Long userId);
 
     @Modifying
+    @Transactional
     @Query("UPDATE Invitation m SET m.isExpired =:isExpired  WHERE m.date = :date")
    void updateExpire(@Param("isExpired") boolean isExpired, @Param("date") LocalDate date);
 }
