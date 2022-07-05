f569fefccbfc6cd825b1f9bf7e70a3717a526b37
Chaeonida <46310555+Chaeonida@users.noreply.github.com>
2022-07-05 20:16:06 +0900

    :recycle: 비밀번호 변경시 확인비밀번호 안받도록 수정

--- src/main/java/com/MakeUs/Waffle/domain/user/dto/UserPasswordRequest.java
+++ src/main/java/com/MakeUs/Waffle/domain/user/dto/UserPasswordRequest.java
@@ -11,24 +11,12 @@ import javax.validation.constraints.Size;
 @Getter
 public class UserPasswordRequest {
     @NotBlank(message = "비밀번호를 입력해 주세요.")
-    @Size(min = 6, message = "비밀번호는 최소 6글자 이상입니다.")
+    @Size(min = 8, message = "비밀번호는 최소 8글자 이상입니다.")
     @Pattern(regexp = "^(?=.*[a-zA-Z])(?=.*[0-9]).{6,20}$", message = "비밀번호는 숫자,영문을 조합해야 합니다.")
     private String nowPassword;
 
     @NotBlank(message = "비밀번호를 입력해 주세요.")
-    @Size(min = 6, message = "비밀번호는 최소 6글자 이상입니다.")
+    @Size(min = 8, message = "비밀번호는 최소 8글자 이상입니다.")
     @Pattern(regexp = "^(?=.*[a-zA-Z])(?=.*[0-9]).{6,20}$", message = "비밀번호는 숫자,영문을 조합해야 합니다.")
     private String newPassword;
-
-    @NotBlank(message = "변경할 비밀번호를 한번 더 입력해 주세요.")
-    @Size(min = 6, message = "비밀번호는 최소 6글자 이상입니다.")
-    @Pattern(regexp = "^(?=.*[a-zA-Z])(?=.*[0-9]).{6,20}$" , message = "변경할 비밀번호와 일치하지 않습니다.")
-    private String newPasswordCheck;
-
-    public boolean isDifferentPassword() {
-        if (!this.newPassword.equals(this.newPasswordCheck)) {
-            throw new NotMatchResourceException(ErrorCode.NOT_SAME_PASSWORD);
-        }
-        return false;
-    }
 }
